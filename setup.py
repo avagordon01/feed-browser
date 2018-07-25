695e6537cfceff2e7c74c82385e729f3e93b5042
Patrick Gordon <patrick@hadean.com>
2018-07-25 23:04:13 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='feed-browser',
-    version='0.1.3',
+    version='0.1.4',
     scripts=['feed-browser/feed-browser'],
     install_requires=[
         'opml',
