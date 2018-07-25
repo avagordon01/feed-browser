56103f5511648dfd8e490d173124e51ec7efa286
Patrick Gordon <patrick@hadean.com>
2018-07-25 22:46:30 +0100

    rename readme for PyPi, bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='feed-browser',
-    version='0.1.1',
+    version='0.1.3',
     scripts=['feed-browser/feed-browser'],
     install_requires=[
         'opml',
