8d7f0bb53e45f5b686939f75e8dd4cead1ec8c3a
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-12-15 18:54:48 +0000

    first go

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+use rss::Channel;
+use chrono::prelude::*;
+use webbrowser;
+
 fn main() {
-    println!("Hello, world!");
+    let mut first = true;
+    fs::read_to_string("/home/patrick/.config/feed-browser/feeds.txt")
+        .expect("Something went wrong reading the file")
+        .lines()
+        .for_each(|url| for item in Channel::from_url(url).unwrap().into_items() {
+            //if DateTime::parse_from_rfc2822(item.pub_date().unwrap()).unwrap() > last_read_date {
+            if first {
+                webbrowser::open(item.link().unwrap()).unwrap();
+                first = false;
+            }
+        });
 }
