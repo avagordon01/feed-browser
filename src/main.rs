a92787fd13a781d4cb2fa0d2569327b0ded7c52a
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-12-16 13:23:18 +0000

    working on date parsing

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,23 @@ fn main() {
         .expect("couldn't find file config.toml");
     let feed_file = xdg_dirs.find_config_file("feeds.txt")
         .expect("couldn't find file feeds.txt");
-    let mut first = true;
+    let mut last_read_string = fs::read_to_string(config_file)
+        .expect("something went wrong reading the file config.toml")
+        .parse::<toml::value::Datetime>().unwrap()
+        .to_string();
+    last_read_string = "2018-12-13T23:01:10-08:00".to_string();
+    let last_read = DateTime::parse_from_rfc3339(&last_read_string).unwrap();
     fs::read_to_string(feed_file)
         .expect("something went wrong reading the file feeds.txt")
         .lines()
         .for_each(|url| for item in Channel::from_url(url).unwrap().into_items() {
-            //if DateTime::parse_from_rfc2822(item.pub_date().unwrap()).unwrap() > last_read_date {
-            if first {
-                webbrowser::open(item.link().unwrap()).unwrap();
-                first = false;
+            match DateTime::parse_from_rfc2822(item.pub_date().unwrap()) {
+                ParseError => println!("good");
+                DateTime<FixedOffset> => println!("good");
+                let updated = item_date > last_read;
+                if updated {
+                    webbrowser::open(item.link().unwrap()).unwrap();
+                }
             }
         });
 }
