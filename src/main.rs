5556c6a3d913c6cb0503cb585d2a0ae4cb288c3b
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-12-16 14:56:50 +0000

    mostly working

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use chrono::prelude::*;
 use webbrowser;
 use xdg;
 use toml;
+use rayon::prelude::*;
 
 fn main() {
     let xdg_dirs = xdg::BaseDirectories::with_prefix("feed-browser").unwrap();
@@ -11,23 +12,34 @@ fn main() {
         .expect("couldn't find file config.toml");
     let feed_file = xdg_dirs.find_config_file("feeds.txt")
         .expect("couldn't find file feeds.txt");
-    let mut last_read_string = fs::read_to_string(config_file)
-        .expect("something went wrong reading the file config.toml")
-        .parse::<toml::value::Datetime>().unwrap()
-        .to_string();
-    last_read_string = "2018-12-13T23:01:10-08:00".to_string();
-    let last_read = DateTime::parse_from_rfc3339(&last_read_string).unwrap();
-    fs::read_to_string(feed_file)
-        .expect("something went wrong reading the file feeds.txt")
-        .lines()
-        .for_each(|url| for item in Channel::from_url(url).unwrap().into_items() {
-            match DateTime::parse_from_rfc2822(item.pub_date().unwrap()) {
-                ParseError => println!("good");
-                DateTime<FixedOffset> => println!("good");
-                let updated = item_date > last_read;
-                if updated {
-                    webbrowser::open(item.link().unwrap()).unwrap();
+    let last_read_string = fs::read_to_string(config_file)
+        .expect("something went wrong reading the file config.toml");
+    let last_read = DateTime::parse_from_rfc3339(&last_read_string.trim()).unwrap();
+    let file_string = fs::read_to_string(feed_file)
+        .expect("something went wrong reading the file feeds.txt");
+    let feed_urls = file_string.par_lines();
+    let items = feed_urls.flat_map(|url| {
+        match Channel::from_url(url) {
+            Ok(items) => {items.into_items()},
+            _ => vec!{},
+        }
+    });
+    let new_items = items.filter(|item| {
+        match item.pub_date() {
+            Some(d) => {
+                match DateTime::parse_from_rfc2822(d) {
+                    Ok(item_date) => item_date > last_read,
+                    _ => false,
                 }
             }
-        });
+            _ => false,
+        }
+    });
+    let new_links = new_items.filter_map(|i| {
+        match i.link() {
+            Some(l) => Some(l.to_owned()),
+            _ => None,
+        }
+    });
+    new_links.for_each(|l| {webbrowser::open(&l).unwrap();});
 }
