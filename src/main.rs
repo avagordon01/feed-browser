9ab6385c7ea3bb1d57ac48fa765ccd79a6b1cf09
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-12-16 00:56:45 +0000

    working with a single feeds file

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,17 @@ use rss::Channel;
 use chrono::prelude::*;
 use webbrowser;
 use xdg;
-use std::iter::FlatMap;
 
 fn main() {
     let xdg_dirs = xdg::BaseDirectories::with_prefix("feed-browser").unwrap();
-    let config_dir = xdg_dirs.find_config_file("config.ini").unwrap();
-    let feed_files = xdg_dirs.list_config_files("feeds.txt");
-    println!("{}", config_dir.to_str().unwrap());
-    let mut first = false;
-    feed_files.into_iter().flat_map(|filename| fs::read_to_string(filename)
-        .expect("Something went wrong reading the file")
-        .lines().cloned()
-        )
+    let config_file = xdg_dirs.find_config_file("config.ini")
+        .expect("couldn't find file config.ini");
+    let feed_file = xdg_dirs.find_config_file("feeds.txt")
+        .expect("couldn't find file feeds.txt");
+    let mut first = true;
+    fs::read_to_string(feed_file)
+        .expect("something went wrong reading the file feeds.txt")
+        .lines()
         .for_each(|url| for item in Channel::from_url(url).unwrap().into_items() {
             //if DateTime::parse_from_rfc2822(item.pub_date().unwrap()).unwrap() > last_read_date {
             if first {
